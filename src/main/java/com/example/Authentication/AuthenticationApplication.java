8f0a80b3a7692f79bf38b469bf74464636dde3bd
Mohit Janghu <mohit@siliconasiaworks.com>
2019-07-05 14:35:26 +0530

    zuul properties and microservices routes

--- src/main/java/com/example/Authentication/AuthenticationApplication.java
+++ src/main/java/com/example/Authentication/AuthenticationApplication.java
@@ -2,12 +2,17 @@ package com.example.Authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthenticationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationApplication.class, args);
 	}
+
 
 }
